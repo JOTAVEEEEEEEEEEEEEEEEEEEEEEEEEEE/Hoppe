4ce2233e606c679b54b1df0030301be4557c5ed7
João Victor <rm87429@estudante.fieb.edu.br>
2023-08-23 18:42:15 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/hoppe/Hoppe/HoppeApplication.java
@@ -0,0 +1,13 @@
+package com.itb.hoppe.Hoppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/hoppe/Hoppe/HoppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.hoppe.Hoppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
